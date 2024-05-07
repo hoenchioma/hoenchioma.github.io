78b9ab58a355e0e6fd578056aabfe310fd988819
Raheeb Hassan <raheeb998@gmail.com>
2024-05-07 07:58:19 +0000

    Update SkillsTab component layout

--- src/components/home/SkillsTab.jsx
+++ src/components/home/SkillsTab.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import SkillsBar from "./SkillsBar";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
 
 function SkillsSection({ skills, isScrolled }) {
   return (
@@ -18,26 +20,20 @@ function SkillsSection({ skills, isScrolled }) {
 
 function SkillsTab({ skills, isScrolled }) {
   return (
-    <div class="card-content">
-      <div class="row text-center">
+    <div class="card-content text-center p-3">
+      <Container center>
+        <Row xs={12} md={6}>
+          <SkillsSection
+            skills={skills}
+            isScrolled={isScrolled}
+          />
+        </Row>
+      </Container>
+      {/* <div class="row text-center">
       <SkillsSection
            skills={skills} />
+      </div> */}
       </div>
-      </div>
-  //   <>
-  //   <Col xs={12} md={6}>
-  //     <SkillsSection
-  //       skills={skills.slice(0, Math.floor(skills.length / 2))}
-  //       isScrolled={isScrolled}
-  //     />
-  //   </Col>
-  //   <Col xs={12} md={6}>
-  //     <SkillsSection
-  //       skills={skills.slice(Math.floor(skills.length / 2), skills.length)}
-  //       isScrolled={isScrolled}
-  //     />
-  //   </Col>
-  // </>
   );
 }
 
